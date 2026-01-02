87e336299a54aa16ad54b625da16e33761d0d04a
inspiration_gx <conservedinnovation@gmail.com>
2026-01-03 00:06:00 +0100

    feat: implemented RefCell

---
+++ smart_pointers/src/refcell.rs
@@ -0,0 +1,111 @@
+use crate::pell::{IPell, Pell as Cell};
+use core::{
+    cell::UnsafeCell,
+    ops::{Deref, DerefMut},
+};
+
+#[derive(Clone, Copy)]
+enum RefState {
+    Unshared,
+    Shared(usize),
+    Exclusive,
+}
+
+struct RefCell<T> {
+    value: UnsafeCell<T>,
+    state: Cell<RefState>,
+}
+
+impl<T> RefCell<T> {
+    fn new(value: T) -> Self {
+        Self {
+            value: UnsafeCell::new(value),
+            state: Cell::new(RefState::Unshared),
+        }
+    }
+
+    fn borrow(&self) -> Option<Ref<'_, T>> {
+        match self.state.get() {
+            RefState::Unshared => {
+                self.state.set(RefState::Shared(1));
+                // Some(unsafe { &*self.value.get() });
+                Some(Ref { refcell: self })
+            }
+            RefState::Shared(n) => {
+                self.state.set(RefState::Shared(n + 1));
+                // Some(unsafe { &*self.value.get() })
+                Some(Ref { refcell: self })
+            }
+
+            RefState::Exclusive => None,
+        }
+    }
+
+    fn borrow_mut(&self) -> Option<RefMut<'_, T>> {
+        if let RefState::Unshared = self.state.get() {
+            self.state.set(RefState::Exclusive);
+            Some(RefMut { refcell: self })
+        } else {
+            None
+        }
+    }
+}
+
+pub struct Ref<'refcell, T> {
+    refcell: &'refcell RefCell<T>,
+}
+
+pub struct RefMut<'refcell, T> {
+    refcell: &'refcell RefCell<T>,
+}
+
+impl<T> Deref for Ref<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.refcell.value.get() }
+    }
+}
+
+impl<T> Deref for RefMut<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.refcell.value.get() }
+    }
+}
+
+impl<T> DerefMut for RefMut<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.refcell.value.get() }
+    }
+}
+
+impl<T> Drop for Ref<'_, T> {
+    fn drop(&mut self) {
+        match self.refcell.state.get() {
+            RefState::Unshared | RefState::Exclusive => {
+                unreachable!()
+            }
+            RefState::Shared(1) => {
+                self.refcell.state.set(RefState::Unshared);
+            }
+            RefState::Shared(n) => {
+                self.refcell.state.set(RefState::Shared(n - 1));
+            }
+        }
+    }
+}
+
+impl<T> Drop for RefMut<'_, T> {
+    fn drop(&mut self) {
+        match self.refcell.state.get() {
+            RefState::Shared(_) | RefState::Unshared => {
+                unreachable!()
+            }
+            RefState::Exclusive => {
+                self.refcell.state.set(RefState::Unshared);
+            }
+        }
+    }
+}
